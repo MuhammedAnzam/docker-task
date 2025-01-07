af389b5d100d38c396df85ad6e75dd1d853fa44f
anzam156 <anzamvalad@gmail.com>
2025-01-07 10:34:12 +0530

    ci/cd edit commmit

--- flasktest.py
+++ flasktest.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-    return "Hello, mahn!How are youuuuuuu......................"
-=======
+
     return "Hello, mahn!How are youuuuuuuuuuuu.................."
->>>>>>> d9baece (ci/cd edit)
 
 if __name__ == "__main__":
     app.run(debug=True)
